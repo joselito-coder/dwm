98b75dd4f6e47c8a7eeed006f0e7822c59418f21
Joselito Rebelo <joselitorebello20@gmail.com>
2022-03-07 16:27:53 +0530

    Added gaps between windows

--- config.h
+++ config.h
@@ -2,10 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 6;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVu Sans:size=13:antialias=true:autohint=true","Font Awesome 5 Free:size:20:antialias=true:autohint:true" };
+static const char *fonts[]          = { "DejaVu Sans:size=13:antialias=true:autohint=true","Font Awesome :size:20:antialias=true:autohint:true" };
 static const char dmenufont[]       = "DejaVu Sans:size=12:antialias=true:autohint=true";
 // Background color
 static const char col_gray1[]       = "#020202";
@@ -59,6 +60,7 @@ static const Layout layouts[] = {
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
+
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
@@ -67,15 +69,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "termite", NULL };
 static const char *firefox[] = {"firefox",NULL};
-
+static const char *lock[] = {"dm-tool","switch-to-greeter"};
 
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ ControlMask|ShiftMask,             XK_f, 	   spawn,          {.v = firefox } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd }  },
+	{ ControlMask|ShiftMask,        XK_f, 	   spawn,          {.v = firefox }  },
+	{ Mod4Mask,			XK_l,	   spawn,	   {.v = lock} 	    },
+	{ MODKEY,                       XK_b,      togglebar,      {0} 	      },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -94,6 +97,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
